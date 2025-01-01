f9b060be35e7495b5a2f15c03dc5d145438a9a09
karan hareshwar sankhe <sankhe00009@gmail.com>
2025-01-01 20:17:12 +0530

    first commit

--- overview/main.py
+++ overview/main.py
@@ -16,6 +16,7 @@ import bs4
 from langchain import hub 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_groq import ChatGroq
@@ -26,13 +27,54 @@ from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
 #load 
 
-laoder = WebBaseLoader(
-    web_path=("",),
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
     bs_kwargs=dict(
-        parse_only = bs4.SoupStrainer(
-            class_=("post-content","post-title","post-header")
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
         )
     ),
 )
+docs = loader.load()
 
+
+#split-Chunking 
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits =  text_splitter.split_documents(docs)
+
+
+# Embed
+model_name = "BAAI/bge-small-en"
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": True}
+hf_embeddings = HuggingFaceBgeEmbeddings(
+    model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+)
+vectorstore = FAISS.from_documents(documents=splits, 
+                                    embedding=hf_embeddings)
+
+retriever = vectorstore.as_retriever()
+#retirve
+
+#prompt
+prompt = hub.pull("rlm/rag-prompt")
+
+#llm 
+llm = ChatGroq(model="llama3-8b-8192", temperature=0)
+
+#post-pro
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+#Question
+print(rag_chain.invoke("what is Task Decomposition?"))
 
